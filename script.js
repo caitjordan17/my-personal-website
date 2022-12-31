66e60737fa5e14cfd167e52fd486455d99d61f1a
Caitlin Jordan <jordan.cait17@gmail.com>
2022-12-31 11:32:59 -0800

    JavaScript click alert added

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const h2 = document.createElement("h2");
 h2.textContent = "Welcome!";
 
 document.querySelector("body").appendChild(h2);
+
+
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('Caitlin has been informed!');
+}
+
+document.addEventListener('click', clickAlert);
